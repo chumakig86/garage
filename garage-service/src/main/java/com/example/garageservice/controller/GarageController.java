d27bf75a38f50d675c4787dd52ef290d0f498c5f
Igor Chumachenko <Igor.Chumachenko@resmed.com>
2020-03-13 02:53:06 -0700

    Refactoring.

--- garage-service/src/main/java/com/example/garageservice/controller/GarageController.java
+++ garage-service/src/main/java/com/example/garageservice/controller/GarageController.java
@@ -2,6 +2,7 @@ package com.example.garageservice.controller;
 
 import com.example.garageservice.model.Garage;
 import com.example.garageservice.repository.GarageRepository;
+import com.example.garageservice.service.GarageService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
@@ -27,11 +28,11 @@ import java.util.Optional;
 @RequestMapping("/")
 @Api(value="Garage Management System", description="Operations pertaining to garage in Garage Management System")
 public class GarageController {
-    private final GarageRepository garageRepository;
+    private final GarageService garageService;
 
     @Autowired
-    public GarageController(GarageRepository garageRepository) {
-        this.garageRepository = garageRepository;
+    public GarageController(GarageService garageService) {
+        this.garageService = garageService;
     }
 
     @ApiOperation(value = "Add a garage")
@@ -40,12 +41,7 @@ public class GarageController {
     public ResponseEntity createGarage(
             @ApiParam(value = "Garage object store in database", required = true)
             @Valid @RequestBody Garage garage) {
-        Optional<Garage> newGarage = garageRepository.findGarageByGarageNumber(garage.getGarageNumber());
-        if (newGarage.isPresent()) {
-            return ResponseEntity.badRequest().body("Garage already exist");
-
-        }
-        return new ResponseEntity<>(garageRepository.save(garage), HttpStatus.CREATED);
+        return garageService.createGarage(garage);
     }
 
     @ApiOperation(value = "View a list of available garages", response = List.class)
@@ -54,38 +50,38 @@ public class GarageController {
             @ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
             @ApiResponse(code = 404, message = "The resource you were trying to reach is not found") })
     @GetMapping("getGarages")
-    public List<Garage> listGarages(){
-        return garageRepository.findAll();
+    public List<Garage> getGarages(){
+        return garageService.listGarages();
     }
 
     @GetMapping(value= "/getbyid/{garage-id}")
     public Optional<Garage> getById(@PathVariable(value= "garage-id") String id) {
-        return garageRepository.findById(id);
+        return garageService.getById(id);
     }
 
     @ApiOperation(value = "Get garage by owners surname")
     @GetMapping(value= "/getbyOwnerSurname/{owner-surname}")
     public List<Garage> getByOwnerSurName(@PathVariable(value= "owner-surname") String surname) {
-        return garageRepository.findGarageByOwnerSurname(surname);
+        return garageService.getByOwnerSurName(surname);
     }
 
     @ApiOperation(value = "Get garage by owners phone number")
     @GetMapping(value= "/getbyOwnerPhoneNumber/{owner-phonenumber}")
     public List<Garage> getByOwnerPhoneNumber(@PathVariable(value= "owner-phonenumber") String phoneNumber) {
-        return garageRepository.findGarageByOwnerPhoneNumber(phoneNumber);
+        return garageService.getByOwnerPhoneNumber(phoneNumber);
     }
 
     @ApiOperation(value = "Get garage by car number")
     @GetMapping(value= "/getbyCarNumber/{carnumber}")
     public List<Garage> getByCarNumber(@PathVariable(value= "carnumber") String carNumber) {
-        return garageRepository.findGarageByCarNumber(carNumber);
+        return garageService.getByCarNumber(carNumber);
     }
 
     @ApiOperation(value = "Get garage by garage number")
     @ApiResponses(value = { @ApiResponse(code = 400, message = "Bad request")})
     @GetMapping(value= "/getbyGarageNumber/{garagenumber}")
     public Optional<Garage> getByGarageNumber(@PathVariable(value= "garagenumber") Integer garageNumber) {
-        return garageRepository.findGarageByGarageNumber(garageNumber);
+        return garageService.getByGarageNumber(garageNumber);
     }
 
     @ApiOperation(value = "Update a garage")
@@ -94,16 +90,13 @@ public class GarageController {
                                     @PathVariable(value = "id") String id,
                                     @ApiParam(value = "Update garage object", required = true)
                                     @Valid @RequestBody Garage garage) {
-        garage.setId(id);
-        garageRepository.save(garage);
-        return garage;
+        return garageService.updateGarage(id, garage);
     }
 
     @ApiOperation(value = "Delete a garage")
     @DeleteMapping("/deleteGarage/{id}")
     public String deleteGarage(@ApiParam(value = "Garage Id from which garage object will delete from database", required = true)
                                @PathVariable(value = "id") String id) {
-        garageRepository.deleteById(id);
-        return "Contact record for employee-id= " + id + " deleted.";
+        return garageService.deleteGarage(id);
     }
 }
---
+++ garage-service/src/main/java/com/example/garageservice/service/GarageService.java
@@ -0,0 +1,69 @@
+package com.example.garageservice.service;
+
+import com.example.garageservice.model.Garage;
+import com.example.garageservice.repository.GarageRepository;
+import io.swagger.annotations.ApiParam;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class GarageService {
+    private final GarageRepository garageRepository;
+
+    @Autowired
+    public GarageService(GarageRepository garageRepository) {
+        this.garageRepository = garageRepository;
+    }
+
+    public ResponseEntity createGarage(Garage garage) {
+    Optional<Garage> newGarage = garageRepository.findGarageByGarageNumber(garage.getGarageNumber());
+    if (newGarage.isPresent()) {
+        return ResponseEntity.badRequest().body("Garage already exist");
+
+    }
+    return new ResponseEntity<>(garageRepository.save(garage), HttpStatus.CREATED);
+}
+
+    public List<Garage> listGarages() {
+        return garageRepository.findAll();
+    }
+
+    public Optional<Garage> getById(String id) {
+        return garageRepository.findById(id);
+    }
+
+    public List<Garage> getByOwnerSurName(String surname) {
+        return garageRepository.findGarageByOwnerSurname(surname);
+    }
+
+    public List<Garage> getByOwnerPhoneNumber(String phoneNumber) {
+        return garageRepository.findGarageByOwnerPhoneNumber(phoneNumber);
+    }
+
+    public List<Garage> getByCarNumber(String carNumber) {
+        return garageRepository.findGarageByCarNumber(carNumber);
+    }
+
+    public Optional<Garage> getByGarageNumber(Integer garageNumber) {
+        return garageRepository.findGarageByGarageNumber(garageNumber);
+    }
+
+    public Garage updateGarage(String id, Garage garage) {
+        garage.setId(id);
+        garageRepository.save(garage);
+        return garage;
+    }
+
+    public String deleteGarage(String id) {
+        garageRepository.deleteById(id);
+        return "Contact record for employee-id= " + id + " deleted.";
+    }
+}
