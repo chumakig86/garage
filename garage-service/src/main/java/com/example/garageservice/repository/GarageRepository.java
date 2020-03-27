926720566139ec6174989632455850271c4f1730
Igor Chumachenko <Igor.Chumachenko@resmed.com>
2020-03-27 02:38:24 -0700

    Removed redundant queries.

--- garage-service/src/main/java/com/example/garageservice/repository/GarageRepository.java
+++ garage-service/src/main/java/com/example/garageservice/repository/GarageRepository.java
@@ -11,12 +11,10 @@ import java.util.Optional;
 @Repository
 public interface GarageRepository extends MongoRepository<Garage, String> {
 
-    @Query(value = "{'owner.surname': ?0}")
     List<Garage> findGarageByOwnerSurname(String ownerSurname);
     @Query(value = "{'contact.phoneNumber': ?0}")
     List<Garage> findGarageByOwnerPhoneNumber(String phoneNumber);
     @Query(value = "{'car.carNumber': ?0}")
     List<Garage> findGarageByCarNumber(String carNumber);
-    @Query(value = "{'garageNumber': ?0}")
     Optional<Garage> findGarageByGarageNumber(Integer garageNumber);
 }
